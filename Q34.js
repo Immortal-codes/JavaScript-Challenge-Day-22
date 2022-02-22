38d29d9d433bae9e5188009e572fd28132a6d8dd
Immortal-codes <rajanraj8979@gmail.com>
2022-02-22 22:10:42 +0530

    first commit

---
+++ Q34.js
@@ -0,0 +1,8 @@
+//Write a JavaScript program to get a random integer in the specified range
+
+//#Source https://bit.ly/2neWfJ2 
+const randomIntegerInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomIntegerInRange(0, 5));
+console.log(randomIntegerInRange(2, 5));
+console.log(randomIntegerInRange(5, -5));
+console.log(randomIntegerInRange(-2, -7));
---
+++ Q35.js
@@ -0,0 +1,10 @@
+//Write a JavaScript program to get an array of given n random integers in the specified range.
+
+//#Source https://bit.ly/2neWfJ2 
+const random_intArray_In_Range = (min, max, n = 1) =>
+  
+Array.from({ length: n }, () => Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+console.log(random_intArray_In_Range(1, 20, 10));
+console.log(random_intArray_In_Range(-10, 10, 5));
